cacff8278d1e49c638752dec34596eb082e6b7cc
RaymartDev <bananamixcraft@gmail.com>
2021-09-29 01:31:11 +0800

    Add project files.

---
+++ NoteTaker/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace NoteTaker
+{
+    partial class Notepad
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.buttonNew = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.titleLabel.Location = new System.Drawing.Point(48, 77);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(39, 21);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Title";
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.messageLabel.Location = new System.Drawing.Point(48, 161);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(71, 21);
+            this.messageLabel.TabIndex = 1;
+            this.messageLabel.Text = "Message";
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(158, 77);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(258, 23);
+            this.titleTextBox.TabIndex = 2;
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Location = new System.Drawing.Point(158, 161);
+            this.messageTextBox.Multiline = true;
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(318, 223);
+            this.messageTextBox.TabIndex = 3;
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AllowUserToAddRows = false;
+            this.dataGrid.AllowUserToDeleteRows = false;
+            this.dataGrid.AllowUserToResizeColumns = false;
+            this.dataGrid.AllowUserToResizeRows = false;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(529, 77);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.ReadOnly = true;
+            this.dataGrid.RowHeadersVisible = false;
+            this.dataGrid.RowTemplate.Height = 25;
+            this.dataGrid.Size = new System.Drawing.Size(240, 307);
+            this.dataGrid.TabIndex = 4;
+            this.dataGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGrid_CellContentClick);
+            // 
+            // buttonNew
+            // 
+            this.buttonNew.Location = new System.Drawing.Point(158, 425);
+            this.buttonNew.Name = "buttonNew";
+            this.buttonNew.Size = new System.Drawing.Size(75, 37);
+            this.buttonNew.TabIndex = 5;
+            this.buttonNew.Text = "New";
+            this.buttonNew.UseVisualStyleBackColor = true;
+            this.buttonNew.Click += new System.EventHandler(this.buttonNew_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(401, 423);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 37);
+            this.buttonSave.TabIndex = 6;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonRead
+            // 
+            this.buttonRead.Location = new System.Drawing.Point(529, 425);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(75, 32);
+            this.buttonRead.TabIndex = 7;
+            this.buttonRead.Text = "Read";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            this.buttonRead.Click += new System.EventHandler(this.buttonRead_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(694, 425);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 37);
+            this.buttonDelete.TabIndex = 8;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(298, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 37);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Sticky Notepad";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Notepad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 528);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonRead);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonNew);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.messageLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Notepad";
+            this.Text = "Notepad";
+            this.Load += new System.EventHandler(this.Notepad_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label messageLabel;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button buttonNew;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonRead;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ NoteTaker/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace NoteTaker
+{
+    public partial class Notepad : Form
+    {
+        private DataTable dataTable;
+        public Notepad()
+        {
+            InitializeComponent();
+        }
+
+        private void Notepad_Load(object sender, EventArgs e)
+        {
+            dataTable = new DataTable();
+            dataTable.Columns.Add("Title", typeof(String));
+            dataTable.Columns.Add("Messages", typeof(String));
+
+            dataGrid.DataSource = dataTable;
+
+            dataGrid.Columns["Messages"].Visible = false;
+            dataGrid.Columns["Title"].Width = 240;
+        }
+
+        private void buttonNew_Click(object sender, EventArgs e)
+        {
+            titleTextBox.Clear();
+            messageTextBox.Clear();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            dataTable.Rows.Add(titleTextBox.Text, messageTextBox.Text);
+
+            titleTextBox.Clear();
+            messageTextBox.Clear();
+        }
+
+        private void buttonRead_Click(object sender, EventArgs e)
+        {
+            if (dataGrid.CurrentCell == null)
+                return;
+
+            int index = dataGrid.CurrentCell.RowIndex;
+
+            if (index < 0)
+                return;
+            titleTextBox.Text = dataTable.Rows[index].ItemArray[0].ToString();
+            messageTextBox.Text = dataTable.Rows[index].ItemArray[1].ToString();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            int index = dataGrid.CurrentCell.RowIndex;
+
+            dataTable.Rows[index].Delete();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+    }
+}
